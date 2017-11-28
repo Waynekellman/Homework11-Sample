931dae9ded11ca986a42034ff7ba1b0a08283fa9
rusili <>
2017-11-28 16:54:56 -0500

    worked on viewholders.

--- app/src/main/java/com/example/rusili/homework11/common/json/JsonResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.rusili.homework11.common.json;
-
-public class JsonResponse {
-	private Menu menu;
-
-	public Menu getMenu () {
-		return menu;
-	}
-}
--- app/src/main/java/com/example/rusili/homework11/common/json/Menu.java
+++
@@ -1,9 +0,0 @@
-package com.example.rusili.homework11.common.json;
-
-public class Menu {
-	private Popup popup;
-
-	public Popup getPopup () {
-		return popup;
-	}
-}
